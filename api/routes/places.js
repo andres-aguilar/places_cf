42b881109ddf7a4bc7d956f2e58b4ef28636bfe2
Andres Aguilar <andresyoshimar@gmail.com>
2018-02-19 23:16:46 -0600

    Agregando un controlador para mejorar la organizacion

--- api/routes/places.js
+++ api/routes/places.js
@@ -1,70 +1,16 @@
 const express = require('express');
 
-const Place = require('../models/Place');
+const placesController = require('../controllers/placesController');
 
 let router = express.Router();
 
 router.route("/")
-  .get((req, res) => {
-    Place.find({})
-    .then(docs => {
-      res.json(docs);
-    }).catch(err => {
-      console.log(err);
-      res.json(err);
-    });
-  })
-  .post((req, res) => {
-    Place.create({
-      title: req.body.title,
-      description: req.body.description,
-      acceptsCreditCard: req.body.acceptsCreditCard,
-      openHour: req.body.openHour,
-      closeHour: req.body.closeHour
-    }).then(doc=>{
-      res.json(doc);
-    }).catch(err => {
-      console.log(err);
-      res.json(err);
-    })
-  });
+  .get(placesController.index)
+  .post(placesController.create);
 
 router.route("/:id")
-  .get((req, res) => {
-    Place.findById(req.params.id)
-    .then(doc => {
-      res.json(doc);
-    }).catch(err => {
-      console.log(err);
-      res.json(err);
-    });
-  })
-  .put((req, res) => {
-    let attributes = ["title", "description", "acceptsCreditCard", "openHour", "closeHour"];
-    let placeParams = {};
-
-    attributes.forEach(attr => {
-      if(Object.prototype.hasOwnProperty.call(req.body, attr)){
-        placeParams[attr] = req.body[attr];
-      }
-    });
-
-    Place.findByIdAndUpdate(req.params.id, placeParams, {new: true})
-    .then(doc => {
-      res.json(doc);
-    }).catch(err => {
-      console.log(err);
-      res.json(err);
-    });
-  })
-  .delete((req, res) => {
-    Place.findByIdAndRemove(req.params.id)
-    .then(doc => {
-      res.json(doc);
-    }).catch(err => {
-      console.log(err);
-      res.json(err);
-    });
-  });
+  .get(placesController.show)
+  .put(placesController.update)
+  .delete(placesController.destroy);
 
 module.exports = router;
